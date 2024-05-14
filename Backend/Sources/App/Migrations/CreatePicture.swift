8f9f2ee79c14fae139b7f2a1b335d5c81856f4bf
julistudy <julistudy@host.fritz.box>
2024-05-14 02:11:40 +0200

    recipe and picture relation rework complete

--- Backend/Sources/App/Migrations/CreatePicture.swift
+++ Backend/Sources/App/Migrations/CreatePicture.swift
@@ -4,16 +4,16 @@ import Fluent
 struct CreatePicture: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
         // Create a new table for 'Coffee'
-        database.schema("picture")
+        database.schema("pictures")
             .id() // Creates an 'id' column that is a UUID and the primary key.
             .field("url", .string, .required) // Creates a 'name' column that is a non-nullable String.
             // relations (only parent relations need that line)
-            .field("coffee_id", .uuid, .required, .references("coffee","id",onDelete: .cascade))
+            .field("coffee_id", .uuid, .required, .references("coffees","id",onDelete: .cascade))
             .create()
     }
 
     func revert(on database: Database) -> EventLoopFuture<Void> {
         // Delete the table if we need to revert the migration
-        database.schema("picture").delete()
+        database.schema("pictures").delete()
     }
 }
