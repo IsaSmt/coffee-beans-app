1296f00f7d151c595fad226548e3d1bb75ce35f6
julistudy <julistudy@host-2.local>
2024-06-07 16:21:43 +0200

    changed some variables to int
    Here what the program usually needs is the key of that entity, which is an int value.
    dont know why it was string though. If needed change it back

--- Backend/API-Vorlage/DataObject/Coffee.cs
+++ Backend/API-Vorlage/DataObject/Coffee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Mail;
@@ -24,15 +25,15 @@ namespace API.DataObject {
 
         [Required]
         // connection between coffee and origin
-        public string Origin { get; set; }
+        public int Origin { get; set; }
 
         // connection between coffee and roastery
         [Required]
-        public string Roastery { get; set; }
+        public int Roastery { get; set; }
 
         // connection between coffee and beantype
         [Required]
-        public string Beantype { get; set; }
+        public int Beantype { get; set; }
 
         [Required]
         // da es sich hier um eine nummer handelt kann min max nicht
