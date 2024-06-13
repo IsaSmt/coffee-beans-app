b7c6bc100ef1b1cc370626d3b44e1baa1081a911
bakhtyari <91656717+bakhtyari@users.noreply.github.com>
2024-06-13 21:02:01 +0200

    test

--- Backend/API-Vorlage/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ Backend/API-Vorlage/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5382d4cd256943af5fd7cfda622231b0604de5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16c7fba1c6d1d1d177610299a11acfd2bb6e5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
