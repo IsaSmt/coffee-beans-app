ec1d4cad2e639fa33171cac0d92f078b1d295337
julistudy <julistudy@host-2.local>
2024-07-07 21:53:17 +0200

    Startup file update
    Changed three coffee entries in our startup.cs file for tomorrows presentation.
    
    via swagger the usual commands seemed to work

--- Backend/API-Vorlage/DataObject/Coffee.cs
+++ Backend/API-Vorlage/DataObject/Coffee.cs
@@ -24,6 +24,8 @@ namespace API.DataObject {
         public string CoffeeDescription { get; set; }
 
         [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
         // connection between coffee and origin
         public string Origin { get; set; }
 
